1aec423d202d960ad076828cbef24217524696d1
blockerabot <blockeraai+githubbot@gmail.com>
2024-10-01 15:55:06 +0330

    chore: update the .gitignore

--- src/test.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-console.log(path.join('./', 'packages/dev-tools'))
-console.log(path.join('./', 'blockera'))
-
-console.log(path.join(path.join('./', 'blockera'), path.join('./', 'packages/dev-tools')))
