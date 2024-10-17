e4bbd0b22badb97be94496862af3193913c3370c
rezaelahidev <rezaelahi.dev@gmail.com>
2024-10-17 15:20:33 +0330

    fix: syntax error

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ export const run = async () => {
                 continue;
             }
 
-            const repoIdMatches = repo.exec(/\/\*\.git/gi);
+            const repoIdMatches = /\/\*\.git/gi.exec(repo);
 
             if(!repoIdMatches || !repoIdMatches[0]){
                 continue;
