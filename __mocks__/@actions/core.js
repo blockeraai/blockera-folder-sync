b4297fc9e2b72fcdb49ed02b122d11f57d9355a0
blockerabot <blockeraai+githubbot@gmail.com>
2024-11-18 09:02:11 +0330

    Update the core mocks

--- __mocks__/@actions/core.js
+++ __mocks__/@actions/core.js
@@ -2,6 +2,7 @@ const core = {
     setFailed: jest.fn(),
     info: jest.fn(),
     debug: jest.fn(),
-    setOutput: jest.fn()
+    setOutput: jest.fn(),
+    getInput: jest.fn(),
 };
 module.exports = core;
