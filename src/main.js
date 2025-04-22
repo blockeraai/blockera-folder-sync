c56930ce4c2b2457493aeac9f52453b2ac9ae220
Reza Elahi <rezaelahi.dev@gmail.com>
2025-04-22 08:43:09 +0330

    fix: undefined errors

--- src/main.js
+++ src/main.js
@@ -17,10 +17,8 @@ const STATUSES = {
 const logInfo = (status, data) => {
     if ('string' === typeof data) {
         console.log(STATUSES[status] + data);
-
         return;
     }
-
     console.log(STATUSES[status], data);
 }
 
@@ -28,16 +26,16 @@ const switchToSyncBranch = async (git, branchName) => {
     logInfo('loading', `Create branch: ${branchName}`);
 
     try {
-        git.checkout(['-b', branchName]);
+        await git.checkout(['-b', branchName]);
     } catch (error) {
-        if (/A branch named '.*' already exists\./gi.test(e.message)) {
+        if (/A branch named '.*' already exists\./gi.test(error.message)) {
             logInfo('loading', `Switch to exists branch ${branchName}`);
             await git.checkout([branchName]);
 
             logInfo('loading', `Git Pull from origin/${branchName}`);
             await git.pull('origin', branchName, ['--no-rebase']);
         } else {
-            throw new Error(error);
+            throw error;
         }
     }
 }
@@ -54,41 +52,35 @@ const commit = async (git, {
         return;
     }
 
-    // Commit changes.
-    await git.add('./*').catch((error) => {
-        logInfo('error', error.message);
-    });
-    await git.commit(`Sync shared packages from ${github.context.repo.repo}`).catch((error) => {
-        logInfo('error', error.message);
-    });
-    logInfo('success', `Commit Changelogs.`);
+    try {
+        // Commit changes.
+        await git.add('./*');
+        await git.commit(`Sync shared packages from ${github.context.repo.repo}`);
+        logInfo('success', `Commit Changelogs.`);
 
-    // Push changes and create PR.
-    await git.push('origin', branchName).catch((error) => {
-        logInfo('error', error.message);
-    });
-    logInfo('success', `Changes pushed to ${repo}`);
+        // Push changes and create PR.
+        await git.push(['--set-upstream', 'origin', branchName]);
+        logInfo('success', `Changes pushed to ${repo}`);
 
-    getOpenedPullRequest().then((data) => {
-        if (!data.length) {
-            try {
-                // Use octokit to create a pull request.
-                const octokit = github.getOctokit(getInput('TOKEN'));
-                octokit.rest.pulls.create({
-                    owner: github.context.repo.owner,
-                    repo: repoIdMatches[1],
-                    title: `Sync package from ${github.context.repo.repo} Repo`,
-                    head: `sync-packages-from-${github.context.repo.repo}`,
-                    base: 'master',
-                    body: `This PR syncs the package from the [${github.context.repo.repo}](https://github.com/blockeraai/${github.context.repo.repo}) repository.`
-                });
+        const openPRs = await getOpenedPullRequest();
+        if (!openPRs.length) {
+            // Use octokit to create a pull request.
+            const octokit = github.getOctokit(getInput('TOKEN'));
+            await octokit.rest.pulls.create({
+                owner: github.context.repo.owner,
+                repo: repoIdMatches[1],
+                title: `Sync package from ${github.context.repo.repo} Repo`,
+                head: branchName,
+                base: 'master',
+                body: `This PR syncs the package from the [${github.context.repo.repo}](https://github.com/blockeraai/${github.context.repo.repo}) repository.`
+            });
 
-                logInfo('success', `Created The Sync package from ${github.context.repo.repo} Repo Pull Request.`);
-            } catch (e) {
-                logInfo('error', e.message);
-            }
+            logInfo('success', `Created The Sync package from ${github.context.repo.repo} Repo Pull Request.`);
         }
-    });
+    } catch (error) {
+        logInfo('error', error.message);
+        throw error;
+    }
 };
 
 /**
@@ -97,119 +89,96 @@ const commit = async (git, {
  * @returns {Promise<void>}
  */
 export const run = async () => {
-    // Set up Git configuration.
-    const git = simpleGit();
+    try {
+        // Set up Git configuration.
+        const git = simpleGit();
 
-    // Apply the user.name and user.email globally or within the repo.
-    await git.addConfig('user.name', getInput('USERNAME'), undefined, {global: true}).catch((error) => {
-        logInfo('error', error.message);
-    });
-    await git.addConfig('user.email', getInput('EMAIL'), undefined, {
-        global: true
-    }).catch((error) => {
-        logInfo('error', error.message);
-    });
+        // Apply the user.name and user.email globally
+        await git.addConfig('user.name', getInput('USERNAME'), undefined, {global: true});
+        await git.addConfig('user.email', getInput('EMAIL'), undefined, {global: true});
 
-    // Get the current repository information from the context.
-    const {owner, repo: repoId} = github.context.repo;
+        // Get the current repository information from the context.
+        const {owner, repo: repoId} = github.context.repo;
 
-    // Construct the HTTPS URL for the current repository.
-    const currentRepoURL = `https://github.com/${owner}/${repoId}.git`;
+        // Construct the HTTPS URL for the current repository.
+        const currentRepoURL = `https://github.com/${owner}/${repoId}.git`;
 
-    // Read blockera-folder-sync.json files from current repository!
-    const packages = await readBlockeraFiles();
-    logInfo('info', `Package paths are ${packages}`);
+        // Read blockera-folder-sync.json files from current repository!
+        const packages = await readBlockeraFiles();
+        logInfo('info', `Package paths are ${JSON.stringify(packages)}`);
 
-    // Clone dependent repos and sync changes.
-    for (const repo in packages) {
-        const packagePaths = packages[repo];
+        // Clone dependent repos and sync changes.
+        for (const repo in packages) {
+            const packagePaths = packages[repo];
 
-        // Skip current repository!
-        if (repo === currentRepoURL) {
-            continue;
-        }
-
-        const repoIdMatches = /\/([a-zA-Z0-9_-]+)\.git$/.exec(repo);
-
-        if (!repoIdMatches || !repoIdMatches[0] || !repoIdMatches[1]) {
-            continue;
-        }
-
-        const repoDir = path.join('./', repoIdMatches[1]);
-        const repositoryURL = repo.replace(/http(?:s|):\/\//gi, '');
-        const repoPath = `https://x-access-token:${getInput(
-            'TOKEN'
-        )}@${repositoryURL}`;
-
-        // Try to clone of repository ...
-        await git.clone(repoPath, repoDir).catch((error) => {
-            logInfo('error', error.message);
-        });
-
-        // Set remote with access token for pushing.
-        await git.cwd(repoDir).catch((error) => {
-            logInfo('error', error.message);
-        });
-        await git.remote([
-            'set-url',
-            'origin',
-            repoPath
-        ]).catch((error) => {
-            logInfo('error', error.message);
-        });
-
-        // Apply the user.name and user.email globally or within the repo.
-        await git.addConfig('user.name', getInput('USERNAME'), undefined, {global: true}).catch((error) => {
-            logInfo('error', error.message);
-        });
-        await git.addConfig('user.email', getInput('EMAIL'), undefined, {
-            global: true
-        }).catch((error) => {
-            logInfo('error', error.message);
-        });
-
-        const branchName = `sync-packages-from-${github.context.repo.repo}`;
-
-        await switchToSyncBranch(git, branchName);
-
-        // Check if there is at least one commit.
-        const log = await git.log();
-        let diff;
-
-        // Sync package directories.
-        for (const packagePath of packagePaths) {
-            if (log.total > 1) {
-                // There are multiple commits, so HEAD^ can be used.
-                diff = await git.diff(['--name-only', 'HEAD^', 'HEAD']);
-            } else if (log.total === 1) {
-                // Only one commit exists, compare against an empty tree (i.e., first commit).
-                diff = await git.diff(['--name-only', 'HEAD']);
-            } else {
-                // No commits, skip diff.
-                info('No commits in the repository.');
-
+            // Skip current repository!
+            if (repo === currentRepoURL) {
                 continue;
             }
 
-            logInfo('loading', 'Repository in progress: ' + repo);
+            const repoIdMatches = /\/([a-zA-Z0-9_-]+)\.git$/.exec(repo);
 
-            const srcDir = path.join('./', packagePath);
-            const destDir = path.join(repoDir, 'packages');
+            if (!repoIdMatches || !repoIdMatches[0] || !repoIdMatches[1]) {
+                continue;
+            }
 
-            // Syncing packages ...
-            await syncDirectories(srcDir, destDir);
-            logInfo('success', `Synced package from ${srcDir} to ${destDir} of ${repo} repository ✅`);
+            const repoDir = path.join('./', repoIdMatches[1]);
+            const repositoryURL = repo.replace(/http(?:s|):\/\//gi, '');
+            const repoPath = `https://x-access-token:${getInput('TOKEN')}@${repositoryURL}`;
+
+            try {
+                // Try to clone repository
+                await git.clone(repoPath, repoDir);
+
+                // Set remote with access token for pushing.
+                await git.cwd(repoDir);
+                await git.remote(['set-url', 'origin', repoPath]);
+
+                // Apply git config in repo directory
+                await git.addConfig('user.name', getInput('USERNAME'), undefined, {global: true});
+                await git.addConfig('user.email', getInput('EMAIL'), undefined, {global: true});
+
+                const branchName = `sync-packages-from-${github.context.repo.repo}`;
+
+                await switchToSyncBranch(git, branchName);
+
+                // Check if there is at least one commit.
+                const log = await git.log();
+
+                // Sync package directories.
+                for (const packagePath of packagePaths) {
+                    if (log.total === 0) {
+                        logInfo('info', 'No commits in the repository.');
+                        continue;
+                    }
+
+                    logInfo('loading', 'Repository in progress: ' + repo);
+
+                    const srcDir = path.join('./', packagePath);
+                    const destDir = path.join(repoDir, 'packages');
+
+                    // Syncing packages ...
+                    await syncDirectories(srcDir, destDir);
+                    logInfo('success', `Synced package from ${srcDir} to ${destDir} of ${repo} repository ✅`);
+                }
+
+                await commit(git, {
+                    repo,
+                    branchName,
+                    repoIdMatches,
+                });
+
+                process.chdir('..');
+            } catch (error) {
+                logInfo('error', `Failed processing repo ${repo}: ${error.message}`);
+                process.chdir('..');
+                continue;
+            }
         }
-
-        await commit(git, {
-            repo,
-            branchName,
-            repoIdMatches,
-        }).catch((error) => {
-            logInfo('error', error.message);
-        });
-
-        process.chdir('..');
+    } catch (error) {
+        logInfo('error', error.message);
+        setFailed(error.message);
+        throw error;
     }
 };
 
